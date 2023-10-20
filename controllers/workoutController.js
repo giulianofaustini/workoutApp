4eaf5e3ab7bf11fa11fdd28b80a931683128e79b
giulianofaustini <139208335+giulianofaustini@users.noreply.github.com>
2023-10-20 11:06:57 +0300

    controllets file

---
+++ controllers/workoutController.js
@@ -0,0 +1,21 @@
+const Workout = require('../models/workoutModel')
+
+
+// get all workouts
+
+
+
+
+// get a single workout
+
+
+
+// POST workouts
+
+
+
+// DELETE a workout
+
+
+
+// UPDATE a workout 
